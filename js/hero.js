8f24a97b31e1597ebc3ff33c15900fcdc1d721b6
wallaskoelho <wallaskoelho@gmail.com>
2023-12-20 07:46:32 -0300

    arquivos script e hero .js criado

---
+++ js/hero.js
@@ -0,0 +1,13 @@
+
+main();
+
+export function main (){
+    console.log("Helo word");
+};
+
+class hero{
+    constructor(name, age, attack){
+        this.name = [Mago, Guerreiro, Monge, Ninja];
+        
+    };
+};
